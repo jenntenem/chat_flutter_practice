af8f6522c805e0a7a45027d1bf6903a56e30566c
Jenniffer Tenempaguay <jenntenem@gmail.com>
2024-01-24 10:49:13 -0500

    AppBar added
    - Avatar and username from chat

--- lib/presentation/screen/chat/chat.screen.dart
+++ lib/presentation/screen/chat/chat.screen.dart
@@ -10,6 +10,17 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Username <3'),
+            centerTitle: false,
+            leading: const Padding(
+              padding: EdgeInsets.all(6.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://cdn.britannica.com/12/215012-004-33C40587/English-actor-Tom-Hiddleston-2018.jpg'),
+              ),
+            )),
+        body: Container());
   }
 }
